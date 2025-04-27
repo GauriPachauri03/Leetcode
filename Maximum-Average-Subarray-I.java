b7265ed1152222eeb73fb79466dd6b599367db70
Gauri-577 <gauripachauri945@gmail.com>
2025-04-27 11:21:50 +0530

    [643] [Time Beats: 45.47%] [Memory Beats: 17.47%] - LeetPush

---
+++ Maximum-Average-Subarray-I.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum=0;
+        for(int i=0;i<=k-1;i++){
+            sum+=nums[i];
+        }
+        double maxSum=sum;
+        for(int i=k;i<nums.length;i++){
+            sum = sum +nums[i]-nums[i-k];
+            maxSum = (double)Math.max(sum,maxSum);
+        }
+        return maxSum/k;
+    }
+}
